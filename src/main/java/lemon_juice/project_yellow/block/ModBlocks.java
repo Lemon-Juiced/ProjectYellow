5a07d32f89fa4afce5833d14ec1cab6f943e1aab
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-06-16 20:36:57 -0400

    Beginnings of Basalt and Marble (Since Vanilla Basalt Exists May Rename)

--- src/main/java/lemon_juice/project_yellow/block/ModBlocks.java
+++ src/main/java/lemon_juice/project_yellow/block/ModBlocks.java
@@ -4,10 +4,10 @@ import lemon_juice.project_yellow.ProjectYellow;
 import lemon_juice.project_yellow.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,7 +17,40 @@ import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ProjectYellow.MOD_ID);
+    
+    // Stone Blocks
+    public static final RegistryObject<Block> BASALT = registerBlock("basalt", () -> new Block(BlockBehaviour.Properties.of().requiresCorrectToolForDrops().strength(3.0F, 6.0F).sound(SoundType.BASALT)));
+    public static final RegistryObject<Block> BASALT_STAIRS = registerBlock("basalt_stairs", () ->  new StairBlock(ModBlocks.BASALT.get().defaultBlockState(), BlockBehaviour.Properties.copy(ModBlocks.BASALT.get())));
+    public static final RegistryObject<Block> BASALT_SLAB = registerBlock("basalt_slab", () ->  new SlabBlock(BlockBehaviour.Properties.copy(ModBlocks.BASALT.get())));
+    public static final RegistryObject<Block> BASALT_WALL = registerBlock("basalt_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(ModBlocks.BASALT.get()).forceSolidOn()));
 
+    public static final RegistryObject<Block> BASALT_BRICKS = registerBlock("basalt_bricks", () -> new Block(BlockBehaviour.Properties.copy(ModBlocks.BASALT.get()).strength(3.5F, 6.0F)));
+    public static final RegistryObject<Block> BASALT_BRICKS_STAIRS = registerBlock("basalt_bricks_stairs", () ->  new StairBlock(ModBlocks.BASALT_BRICKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(ModBlocks.BASALT_BRICKS.get())));
+    public static final RegistryObject<Block> BASALT_BRICKS_SLAB = registerBlock("basalt_bricks_slab", () ->  new SlabBlock(BlockBehaviour.Properties.copy(ModBlocks.BASALT_BRICKS.get())));
+    public static final RegistryObject<Block> BASALT_BRICKS_WALL = registerBlock("basalt_bricks_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(ModBlocks.BASALT_BRICKS.get()).forceSolidOn()));
+
+    public static final RegistryObject<Block> COBBLED_BASALT = registerBlock("cobbled_basalt", () -> new Block(BlockBehaviour.Properties.copy(ModBlocks.BASALT.get()).strength(3.5F, 6.0F)));
+    public static final RegistryObject<Block> COBBLED_BASALT_STAIRS = registerBlock("cobbled_basalt_stairs", () ->  new StairBlock(ModBlocks.COBBLED_BASALT.get().defaultBlockState(), BlockBehaviour.Properties.copy(ModBlocks.COBBLED_BASALT.get())));
+    public static final RegistryObject<Block> COBBLED_BASALT_SLAB = registerBlock("cobbled_basalt_slab", () ->  new SlabBlock(BlockBehaviour.Properties.copy(ModBlocks.COBBLED_BASALT.get())));
+    public static final RegistryObject<Block> COBBLED_BASALT_WALL = registerBlock("cobbled_basalt_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(ModBlocks.COBBLED_BASALT.get()).forceSolidOn()));
+
+    public static final RegistryObject<Block> MARBLE = registerBlock("marble", () -> new Block(BlockBehaviour.Properties.of().requiresCorrectToolForDrops().strength(3.0F, 6.0F).sound(SoundType.CALCITE)));
+    public static final RegistryObject<Block> MARBLE_STAIRS = registerBlock("marble_stairs", () ->  new StairBlock(ModBlocks.MARBLE.get().defaultBlockState(), BlockBehaviour.Properties.copy(ModBlocks.MARBLE.get())));
+    public static final RegistryObject<Block> MARBLE_SLAB = registerBlock("marble_slab", () ->  new SlabBlock(BlockBehaviour.Properties.copy(ModBlocks.MARBLE.get())));
+    public static final RegistryObject<Block> MARBLE_WALL = registerBlock("marble_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(ModBlocks.MARBLE.get()).forceSolidOn()));
+
+    public static final RegistryObject<Block> MARBLE_BRICKS = registerBlock("marble_bricks", () -> new Block(BlockBehaviour.Properties.copy(ModBlocks.MARBLE.get()).strength(3.5F, 6.0F)));
+    public static final RegistryObject<Block> MARBLE_BRICKS_STAIRS = registerBlock("marble_bricks_stairs", () ->  new StairBlock(ModBlocks.MARBLE_BRICKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(ModBlocks.MARBLE_BRICKS.get())));
+    public static final RegistryObject<Block> MARBLE_BRICKS_SLAB = registerBlock("marble_bricks_slab", () ->  new SlabBlock(BlockBehaviour.Properties.copy(ModBlocks.MARBLE_BRICKS.get())));
+    public static final RegistryObject<Block> MARBLE_BRICKS_WALL = registerBlock("marble_bricks_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(ModBlocks.MARBLE_BRICKS.get()).forceSolidOn()));
+    
+    public static final RegistryObject<Block> COBBLED_MARBLE = registerBlock("cobbled_marble", () -> new Block(BlockBehaviour.Properties.copy(ModBlocks.MARBLE.get()).strength(3.5F, 6.0F)));
+    public static final RegistryObject<Block> COBBLED_MARBLE_STAIRS = registerBlock("cobbled_marble_stairs", () ->  new StairBlock(ModBlocks.COBBLED_MARBLE.get().defaultBlockState(), BlockBehaviour.Properties.copy(ModBlocks.COBBLED_MARBLE.get())));
+    public static final RegistryObject<Block> COBBLED_MARBLE_SLAB = registerBlock("cobbled_marble_slab", () ->  new SlabBlock(BlockBehaviour.Properties.copy(ModBlocks.COBBLED_MARBLE.get())));
+    public static final RegistryObject<Block> COBBLED_MARBLE_WALL = registerBlock("cobbled_marble_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(ModBlocks.COBBLED_MARBLE.get()).forceSolidOn()));
+
+
+    // Ores
     public static final RegistryObject<Block> PERIDOT_ORE = registerBlock("peridot_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.IRON_ORE).requiresCorrectToolForDrops().strength(3.0F, 3.0F)));
     public static final RegistryObject<Block> DEEPSLATE_PERIDOT_ORE = registerBlock("deepslate_peridot_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(PERIDOT_ORE.get()).requiresCorrectToolForDrops().strength(4.5F, 3.0F)));
 
--- src/main/java/lemon_juice/project_yellow/creativetab/ModCreativeTab.java
+++ src/main/java/lemon_juice/project_yellow/creativetab/ModCreativeTab.java
@@ -97,6 +97,36 @@ public class ModCreativeTab {
             event.accept(ModBlocks.DEEPSLATE_RUBY_ORE.get());
             event.accept(ModBlocks.SAPPHIRE_ORE.get());
             event.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+
+            event.accept(ModBlocks.BASALT.get());
+            event.accept(ModBlocks.BASALT_SLAB.get());
+            event.accept(ModBlocks.BASALT_STAIRS.get());
+            event.accept(ModBlocks.BASALT_WALL.get());
+
+            event.accept(ModBlocks.BASALT_BRICKS.get());
+            event.accept(ModBlocks.BASALT_BRICKS_SLAB.get());
+            event.accept(ModBlocks.BASALT_BRICKS_STAIRS.get());
+            event.accept(ModBlocks.BASALT_BRICKS_WALL.get());
+
+            event.accept(ModBlocks.COBBLED_BASALT.get());
+            event.accept(ModBlocks.COBBLED_BASALT_SLAB.get());
+            event.accept(ModBlocks.COBBLED_BASALT_STAIRS.get());
+            event.accept(ModBlocks.COBBLED_BASALT_WALL.get());
+
+            event.accept(ModBlocks.MARBLE.get());
+            event.accept(ModBlocks.MARBLE_SLAB.get());
+            event.accept(ModBlocks.MARBLE_STAIRS.get());
+            event.accept(ModBlocks.MARBLE_WALL.get());
+
+            event.accept(ModBlocks.MARBLE_BRICKS.get());
+            event.accept(ModBlocks.MARBLE_BRICKS_SLAB.get());
+            event.accept(ModBlocks.MARBLE_BRICKS_STAIRS.get());
+            event.accept(ModBlocks.MARBLE_BRICKS_WALL.get());
+
+            event.accept(ModBlocks.COBBLED_MARBLE.get());
+            event.accept(ModBlocks.COBBLED_MARBLE_SLAB.get());
+            event.accept(ModBlocks.COBBLED_MARBLE_STAIRS.get());
+            event.accept(ModBlocks.COBBLED_MARBLE_WALL.get());
         }
     }
 
